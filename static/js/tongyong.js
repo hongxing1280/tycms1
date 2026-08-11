e2c8331a7d1c154da9ad68ef1f96cec241ccd478
hongxing1280 <55313982+hongxing1280@users.noreply.github.com>
2026-08-11 15:07:07 +0800

    新建
    8.11

---
+++ static/js/tongyong.js
@@ -0,0 +1,11 @@
+var thisurl = window.location.host;
+
+var webname = "天津市天展科技有限公司南明分公司";
+var webdizhi = "贵州省贵阳市南明区中华南路街道富水南路196号贵州国际商城4号主塔楼4单元35层5号[中南办事处]";
+var webhaoma = "";
+var webemail = "";
+var webbeian = "津ICP备2024012283号-1 ";
+var webqq = "";
+var webyoubian = "";
+
+
